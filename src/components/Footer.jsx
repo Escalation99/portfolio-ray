f735595976af5a0ced025d64840204501762d01d
Ray <raytommy1234@gmail.com>
2020-08-22 14:31:35 +0800

    perbaiki footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,27 +3,27 @@ import React from 'react';
 function Footer() {
     return (
         <>
-            <footer class="footer text-center">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-4 mb-5 mb-lg-0">
-                            <h4 class="text-uppercase mb-4">Location</h4>
-                            <p class="lead mb-0">
+            <footer className="footer text-center">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-4 mb-5 mb-lg-0">
+                            <h4 className="text-uppercase mb-4">Location</h4>
+                            <p className="lead mb-0">
                                 @rayt16
                             <br />
                                 Jakarta, Indonesia
                         </p>
                         </div>
-                        <div class="col-lg-4 mb-5 mb-lg-0">
-                            <h4 class="text-uppercase mb-4">Around the Web</h4>
-                            <a class="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com"><i class="fab fa-fw fa-facebook-f"></i></a>
-                            <a class="btn btn-outline-light btn-social mx-1" href="https://www.twitter.com"><i class="fab fa-fw fa-twitter"></i></a>
-                            <a class="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com"><i class="fab fa-fw fa-linkedin-in"></i></a>
-                            <a class="btn btn-outline-light btn-social mx-1" href="https://www.dribbble.com"><i class="fab fa-fw fa-dribbble"></i></a>
+                        <div className="col-lg-4 mb-5 mb-lg-0">
+                            <h4 className="text-uppercase mb-4">Around the Web</h4>
+                            <a className="btn btn-outline-light btn-social mx-1" href="https://www.facebook.com"><i className="fab fa-fw fa-facebook-f"></i></a>
+                            <a className="btn btn-outline-light btn-social mx-1" href="https://www.twitter.com"><i className="fab fa-fw fa-twitter"></i></a>
+                            <a className="btn btn-outline-light btn-social mx-1" href="https://www.linkedin.com"><i className="fab fa-fw fa-linkedin-in"></i></a>
+                            <a className="btn btn-outline-light btn-social mx-1" href="https://www.dribbble.com"><i className="fab fa-fw fa-dribbble"></i></a>
                         </div>
-                        <div class="col-lg-4">
-                            <h4 class="text-uppercase mb-4">About Freelancer</h4>
-                            <p class="lead mb-0">
+                        <div className="col-lg-4">
+                            <h4 className="text-uppercase mb-4">About Freelancer</h4>
+                            <p className="lead mb-0">
                                 <small>Freelance is a free to use, MIT licensed Bootstrap theme created by
                             <a href="http://startbootstrap.com">Start Bootstrap</a>
                                     .</small>
@@ -32,12 +32,12 @@ function Footer() {
                     </div>
                 </div>
             </footer>
-            <div class="copyright py-4 text-center text-white">
-                <div class="container"><small>Copyright © Loker Programmer 2020</small></div>
+            <div className="copyright py-4 text-center text-white">
+                <div className="container"><small>Copyright © Loker Programmer 2020</small></div>
             </div>
 
-            <div class="scroll-to-top d-lg-none position-fixed">
-                <a class="js-scroll-trigger d-block text-center text-white rounded" href="#page-top"><i class="fa fa-chevron-up"></i></a>
+            <div className="scroll-to-top d-lg-none position-fixed">
+                <a className="js-scroll-trigger d-block text-center text-white rounded" href="#page-top"><i className="fa fa-chevron-up"></i></a>
             </div>
         </>
 
