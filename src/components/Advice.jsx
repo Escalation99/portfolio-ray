767afa1b6d3b7da20bbbc8a62d4f7a42071ceff6
Ray <raytommy1234@gmail.com>
2020-08-22 14:23:19 +0800

    udah perbaiki advice

--- src/components/Advice.jsx
+++ src/components/Advice.jsx
@@ -27,7 +27,7 @@ class Advice extends Component {
         return (
             <div className="copyright py-4 text-center text-white">
                 <div className="container">
-                    <p className="lead mt-3">Be the best of the best</p>
+                    <p className="lead mt-3">{this.state.advice}</p>
                     <p>- Me.</p>
                     <button className="btn btn-primary" onClick={this.handleClick}>
                         <i className="fas fa-sync fa-fw"></i>
