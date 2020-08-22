048eb05e66fa9f5ca04b5f478cd2c393f37d5748
Ray <raytommy1234@gmail.com>
2020-08-22 14:21:17 +0800

    tambahkan advice

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./components/Hero.jsx"
 import PortfolioList from './components/PortfolioList.jsx';
 import About from './components/About.jsx';
 import Contact from './components/Contact.jsx';
+import Advice from './components/Advice.jsx';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <PortfolioList />
       <About />
       <Contact />
+      <Advice />
     </div>
   );
 }
