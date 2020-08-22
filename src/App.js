0d9bd0029c1207913d2cf8c32195f2649c709abb
Ray <raytommy1234@gmail.com>
2020-08-22 13:18:55 +0800

    uda kelar contact

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Navbar from "./components/Navbar.jsx"
+import Hero from "./components/Hero.jsx"
+import PortfolioList from './components/PortfolioList.jsx';
+import About from './components/About.jsx';
+import Contact from './components/Contact.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Portfolioku</h1>
+    <div id="page-top">
+      <Navbar />
+      <Hero />
+      <PortfolioList />
+      <About />
+      <Contact />
     </div>
   );
 }
